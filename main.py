017aa5809c1a950bf3ff98cdf197410e18e6bcb6
ga48duv <ga48duv@mytum.de>
2022-08-09 12:52:13 +0200

    other codon table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+from Bio.Data.CodonTable import TranslationError
 from Bio.SeqRecord import SeqRecord
 import argparse
 
@@ -7,7 +8,12 @@ def fna_2_faa(fna_filepath: str, faa_filepath: str):
     with open(faa_filepath, 'w') as aa_fa:
         for dna_record in SeqIO.parse(fna_filepath, 'fasta'):
             # generate all translation frames
-            aa_seqs = dna_record.seq.translate(cds=True, stop_symbol="")
+            try:
+                aa_seqs = dna_record.seq.translate(cds=True, table=11, stop_symbol="")
+            except TranslationError:
+                aa_seqs = dna_record.seq.translate(cds=True, table=4, stop_symbol="")
+            except:
+                print("funny!")
 
             # write new record
             aa_record = SeqRecord(aa_seqs, id=dna_record.id, description="")
@@ -21,4 +27,3 @@ if __name__ == '__main__':
     parser.add_argument('faa', type=str)
     args = parser.parse_args()
     fna_2_faa(args.fna, args.faa)
-
